e0a741242c39578becfeb5c9366021a2e6c02579
WarmEpoch <1523230910@qq.com>
2022-08-28 12:12:44 +0800

    0827

--- src/App.vue
+++ src/App.vue
@@ -13,19 +13,22 @@ import 'swiper/css'
 const swipers = ref(null)
 const toImg = ref(null)
 const imgs = ref([])
+const creates = ref([])
+const preView = ref(false)
 const log = ref()
 const logSrc = ref()
-const loading = ref()
 const dialogs = ref({
   dialog: false,
-  index: 0,
+  index: '0',
   json: {},
   check: () => {
     imgs.value[dialogs.value.index] = {...imgs.value[dialogs.value.index],...dialogs.value.json}
+    console.log(imgs.value[dialogs.value.index])
     dialogs.value.dialog = false
   },
   show: (json,index) => {
     dialogs.value.json = JSON.parse(JSON.stringify(json))
+    console.log(dialogs.value.json)
     dialogs.value.index = index
     dialogs.value.dialog = true
   },
@@ -34,76 +37,91 @@ const dialogs = ref({
   }
 })
 
-const mark = ref([
+const marks = ref([
   {
     name: '徕卡',
     val: 'leica',
+    ratio: '1 / 1',
   },
   {
     name: '哈苏',
     val: 'hasu',
+    ratio: '1 / 1',
   },
   {
     name: '阿莱',
     val: 'arri',
+    ratio: '3 / 2',
   },
   {
     name: '蔡司',
     val: 'zeiss',
+    ratio: '1 / 1',
   },
   {
     name: '富士',
     val: 'fujifilm',
+    ratio: '3 / 2',
   },
   {
     name: '索尼',
     val: 'sony',
+    ratio: '3 / 2',
   },
   {
     name: '佳能',
     val: 'canon',
+    ratio: '3 / 2',
   },
   {
     name: '尼康',
     val: 'nikon',
+    ratio: '3 / 2',
   },
   {
     name: 'FotorGear',
     val: 'fotorgear',
+    ratio: '1 / 1',
   },
   {
     name: 'SIGMA',
     val: 'sigma',
+    ratio: '3 / 2',
   },
   {
     name: '宾得',
     val: 'pentax',
+    ratio: '3 / 2',
   },
   {
     name: '奥林巴斯',
     val: 'olympus',
+    ratio: '3 / 2',
   },
   {
     name: 'TAMRON',
     val: 'tamron',
+    ratio: '3 / 2',
   },
   {
     name: 'RICOH',
     val: 'ricoh',
+    ratio: '3 / 2',
   },
   {
     name: '苹果',
     val: 'apple',
+    ratio: '1 / 1',
   },
 ])
 
 
 const ImgChange = async (uploadFileRaw) => {
-  loading.value = ElLoading.service({
+  let load = ElLoading.service({
     lock: true,
     text: '图片加载中',
   })
-
+  
   let blob = URL.createObjectURL(uploadFileRaw)
 
   const heic = await heic2any({
@@ -116,29 +134,34 @@ const ImgChange = async (uploadFileRaw) => {
   .catch(function (x) {
     return false
   })
+
+  let createHW = new Image()
+  createHW.src = heic || blob
   
   // logSrc.value = heic ? heic : blob
 
   await exifr.parse(blob).then(output => {
     console.log(output)
+    let markIndex = output.Make ? marks.value.findIndex(item => item.val == output.Make.toLowerCase()) : -1
+    let mark = markIndex >= 0 ? markIndex : 0
     // log.value = output
     imgs.value.unshift({
       name: uploadFileRaw.name.split('.')[0],
-      src: heic ? heic : blob,
-      mark: output.Make && mark.value.findIndex(item => item.val == output.Mark) >= 0 ? output.Make.toLowerCase() : 'leica',
-      height: output?.Orientation?.indexOf('90') > 0 || output?.Orientation?.indexOf('270') > 0 ? output.ExifImageWidth || output.ImageWidth : output.ExifImageHeight || output.ImageHeight,
-      width: output?.Orientation?.indexOf('90') > 0 || output?.Orientation?.indexOf('270') > 0 ? output.ExifImageHeight || output.ImageHeight : output.ExifImageWidth || output.ImageWidth,
+      src: heic || blob,
+      mark,
+      height: createHW.height,
+      width: createHW.width,
       model: output.Model || 'Immers',
       date: output.GPSLatitudeRef && output.CreateDate ? format(output.CreateDate) : '',
       focal: output?.FocalLengthIn35mmFormat || output?.FocalLength ? `${output?.FocalLengthIn35mmFormat || output?.FocalLength || false}mm f/${ output?.FNumber || false } ${output.ExposureTime ? output.ExposureTime >= 1 ? output.ExposureTime : `1/${1 / output.ExposureTime}` : false} ISO${ output?.ISO || false }` : 'ImmersMark',
       itude: output.GPSLatitudeRef && output.GPSLatitudeRef ? `${output.GPSLatitude[0]}° ${output.GPSLatitude[1]}' ${Math.round(output.GPSLatitude[2])}"${output.GPSLatitudeRef} ${output.GPSLongitude[0]}° ${output.GPSLongitude[1]}' ${Math.round(output.GPSLongitude[2])}"${output.GPSLongitudeRef}` : output.CreateDate ?  format(output.CreateDate) : format(new Date()),
     })
-    console.log('unshift')
+    console.log(imgs.value[0])
   })
 
-  console.log('loaded')
 
-  loading.value.close()
+  load.close()
+
 
   return false
 }
@@ -158,94 +181,98 @@ const format = (Date) => {
   return Y + '.' + M + '.' + D + ' ' + H + ':' + Mi + ':' + S;
 }
 
-const save = () =>{
-  loading.value = ElLoading.service({
+const Create = () =>{
+  let load = ElLoading.service({
     lock: true,
-    text: '图片保存中',
+    text: '图片生成中',
   })
   document.querySelectorAll('.swiper-slide').forEach(async (el,index) => {
-    let div = document.createElement('div')
-    div.style.width = `${imgs.value[index].width}px`
-    div.innerHTML = el.querySelector('div').innerHTML
+    console.log(imgs.value[index])
+    let div = el.querySelector('.mark').cloneNode(true)
+    div.style.zoom = 'unset'
     toImg.value.append(div)
-
+    let canvas = document.createElement('canvas')
+    canvas.width = imgs.value[index].width
+    canvas.height = imgs.value[index].height + div.clientHeight
+    toImg.value.append(canvas)
+    let cancon = canvas.getContext('2d')
+    let img = new Image()
+    img.src = imgs.value[index].src
+    img.onload =  () => {
+      cancon.drawImage(img,0,0)
+    }
+    
     await domtoimage.toBlob(div).then(blob => {
-        let canvas = document.createElement('canvas')
-        canvas.width = div.clientWidth
-        canvas.height = div.clientHeight
         div.remove()
-        toImg.value.append(canvas)
-        let cancon = canvas.getContext('2d')
         let mark = new Image()
         mark.src = URL.createObjectURL(blob)
         mark.onload = () => {
-          cancon.drawImage(mark,0,0)
-          mark = false
-          if(img) return false
-          let link = document.createElement('a')
-          link.download = `${imgs.value[index].name}-ImmersMark.png`
-          link.href = canvas.toDataURL("image/png")
-          link.click()
-          link.remove()
-          canvas.remove()
-        }
-        let img = new Image()
-        img.src = imgs.value[index].src
-        img.onload =  () => {
-          cancon.drawImage(img,0,0)
-          img = false
-          if(mark) return false
-          let link = document.createElement('a')
-          link.download = `${imgs.value[index].name}-ImmersMark.png`
-          link.href = canvas.toDataURL("image/png")
-          link.click()
-          link.remove()
+          cancon.drawImage(mark,0,imgs.value[index].height)
+          creates.value.unshift(canvas.toDataURL("image/png"))
           canvas.remove()
         }
     })
 
 
     if(index >= document.querySelectorAll('.swiper-slide').length - 1){
-      loading.value.close()
+      load.close()
+      preView.value = true
     }
   })
+}
+
+const PreViewClose = () => {
+  creates.value = []
+}
+
+const IconChange = (uploadFileRaw) => {
+  let createHw = new Image()
+  createHw.src = URL.createObjectURL(uploadFileRaw)
+  createHw.onload = () => {
+    marks.value.unshift({
+      name: uploadFileRaw.name.split('.')[0],
+      val: createHw.src,
+      ratio: `${createHw.width} / ${createHw.height}`,
+      custom: true,
+    })
+    dialogs.value.json.mark = 0
+  }
+  
+  return false
 }
 
 
 </script>
 <template>
-
   <el-card>
     <template #header>
       <el-image src="//web.immers.icu/assets/logo.svg" />
-      <el-upload class="avatar-uploader" :before-upload="ImgChange" :show-file-list="false" accept="image" multiple>
+      <el-upload :before-upload="ImgChange" :show-file-list="false" accept="image" multiple>
         <template #trigger>
-          <el-button type="primary" >选择</el-button>
+          <el-button type="primary">选择</el-button>
         </template>
         <template #default>
-          <el-button type="success" :disabled="!imgs.length" @click="save">保存</el-button>
+          <el-button type="success" :disabled="!imgs.length" @click="Create">生成</el-button>
           <a href="https://mp.weixin.qq.com/mp/appmsgalbum?__biz=Mzg3MTgwNzU0NA==&action=getalbum&album_id=2544483400624160768#wechat_redirect" target="_blank"><el-button>教程</el-button></a>
         </template>
       </el-upload>
     </template>
     <swiper ref="swipers">
       <swiper-slide v-for="(img, index) of imgs" :key="index">
-        <div :style="{width: img.width + 'px',zoom: swipers.$el.clientWidth / img.width}">
         <el-popconfirm title="删除此张照片?" confirm-button-text="是的" cancel-button-text="取消" @confirm="dialogs.delete(index)">
           <template #reference>
             <el-image :src="img.src" />
           </template>
         </el-popconfirm>
-          <div @click="dialogs.show(img,index)" :style="{display: 'flex',justifyContent: 'space-between',padding: `${img.width > img.height ? img.width * 0.015 + 'px ' + img.width * 0.038 + 'px ' + img.width * 0.022 + 'px ' + img.width * 0.0385 + 'px' : img.width * 0.032 + 'px ' + img.width * 0.048 + 'px ' + img.width * 0.042 + 'px ' + img.width * 0.05 + 'px'}`,background: '#ffffff',fontSize: img.width > img.height ? img.width * 0.018 + 'px'  : img.width * 0.033 + 'px'}">
-            <div style="display: flex;flex-flow: column wrap;">
-              <p :style="{fontFamily: 'semibold',fontSize:  img.width > img.height ? '.86em' : '.82em',lineHeight: img.width > img.height ? '2.2em' : '1.9em'}">{{ img.model }}</p>
-              <p style="font-family: 'regular';font-size: .69em;color: #818185;">{{ img.date }}</p>
-            </div>
-            <div style="display: flex;flex-flow: column wrap;position: relative;">
-              <el-image :src="`//web.immers.icu/assets/${img.mark}.svg`" :style="{position: 'absolute',top: `${img.width > img.height ? img.width * 0.0103 + 'px' : img.width * 0.014 + 'px'}`,bottom: `${img.width > img.height ? img.width * 0.0028 + 'px' : img.width * 0.0045 + 'px'}`,left: `-${img.width > img.height ? img.width * 0.01 + 'px' : img.width * 0.018 + 'px'}`,transform: 'translateX(-100%)',paddingRight: `${img.width > img.height ? img.width * 0.01 + 'px' : img.width * 0.018 + 'px'}`,borderRight: `solid ${img.width * 0.001}px #ccc`}" ></el-image>
-              <p :style="{fontFamily: 'semibold',fontSize: img.width > img.height ? '.84em' : '.78em',lineHeight: img.width > img.height ? '2.15em' : '2em'}">{{ img.focal }}</p>
-              <p style="font-family: 'regular';font-size: .67em;color: #7f7f7f;">{{ img.itude }}</p>
-            </div>
+        <div @click="dialogs.show(img,index)" class="mark" :style="{boxSizing: 'border-box',width: img.width + 'px',zoom: swipers.$el.clientWidth / img.width,display: 'flex',justifyContent: 'space-between',padding: `${img.width > img.height ? img.width * 0.015 + 'px ' + img.width * 0.038 + 'px ' + img.width * 0.022 + 'px ' + img.width * 0.0385 + 'px' : img.width * 0.032 + 'px ' + img.width * 0.048 + 'px ' + img.width * 0.042 + 'px ' + img.width * 0.05 + 'px'}`,background: '#ffffff',fontSize: img.width > img.height ? img.width * 0.018 + 'px'  : img.width * 0.033 + 'px'}">
+          <div style="display: flex;flex-flow: column wrap;">
+            <p :style="{fontWeight: 'bold',fontSize:  img.width > img.height ? '.86em' : '.82em',lineHeight: img.width > img.height ? '2.2em' : '1.9em'}">{{ img.model }}</p>
+            <p style="font-size: .69em;color: #818185;">{{ img.date }}</p>
+          </div>
+          <div style="display: flex;flex-flow: column wrap;position: relative;">
+            <el-image :src="marks[img.mark]?.custom ? marks[img.mark].val : `//web.immers.icu/assets/${marks[img.mark].val}.svg`" :style="{position: 'absolute',top: `${img.width > img.height ? img.width * 0.0103 + 'px' : img.width * 0.014 + 'px'}`,bottom: `${img.width > img.height ? img.width * 0.0028 + 'px' : img.width * 0.0045 + 'px'}`,left: `-${img.width > img.height ? img.width * 0.01 + 'px' : img.width * 0.018 + 'px'}`,transform: 'translateX(-100%)',paddingRight: `${img.width > img.height ? img.width * 0.01 + 'px' : img.width * 0.018 + 'px'}`,borderRight: `solid ${img.width * 0.001}px #ccc`,aspectRatio: marks[img.mark].ratio}" ></el-image>
+            <p :style="{fontWeight: 'bold',fontSize: img.width > img.height ? '.84em' : '.78em',lineHeight: img.width > img.height ? '2.15em' : '2em'}">{{ img.focal }}</p>
+            <p style="font-size: .67em;color: #7f7f7f;">{{ img.itude }}</p>
           </div>
         </div>
       </swiper-slide>
@@ -256,32 +283,46 @@ const save = () =>{
   </el-card>
   <el-dialog v-model="dialogs.dialog" width="85%" title="自定义">
     <el-form :model="dialogs.json" label-width="auto" label-position="right">
-    <el-form-item label="ICON">
-      <el-select v-model="dialogs.json.mark" placeholder="Select" size="large">
-        <el-option v-for="item of mark" :key="item.val" :label="item.name" :value="item.val" />
-      </el-select>
-    </el-form-item>
-    <el-form-item label="主标题">
-      <el-input v-model="dialogs.json.model" placeholder="左上角" />
-    </el-form-item>
-    <el-form-item label="副主标题">
-      <el-input v-model="dialogs.json.focal" placeholder="右上角" />
-    </el-form-item>
-    <el-form-item label="次标题">
-      <el-input v-model="dialogs.json.date" placeholder="左下角" />
-    </el-form-item>
-    <el-form-item label="副次标题">
-      <el-input v-model="dialogs.json.itude" placeholder="右下角" />
-    </el-form-item>
-    <el-form-item>
-      <el-button type="primary" @click="dialogs.check">确定</el-button>
-    </el-form-item>
-  </el-form>
-    <!-- <template #footer>
-      <span class="dialog-footer">
+      <el-form-item label="ICON">
+        <el-select v-model="dialogs.json.mark" placeholder="Select" size="large">
+          <el-option v-for="(item,index) of marks" :key="item.index" :label="item.name" :value="index" />
+        </el-select>
+        <el-upload :before-upload="IconChange" :show-file-list="false">
+          <template #trigger>
+            <el-button type="primary" size="large" style="margin-left: 1rem;">选择</el-button>
+          </template>
+        </el-upload>
+      </el-form-item>
+      <el-form-item label="宽度">
+        <el-input v-model.number="dialogs.json.width" placeholder="图片宽度" />
+      </el-form-item>
+      <el-form-item label="高度">
+        <el-input v-model.number="dialogs.json.height" placeholder="图片高度" />
+      </el-form-item>
+      <el-form-item label="主标题">
+        <el-input v-model="dialogs.json.model" placeholder="左上角" />
+      </el-form-item>
+      <el-form-item label="副主标题">
+        <el-input v-model="dialogs.json.focal" placeholder="右上角" />
+      </el-form-item>
+      <el-form-item label="次标题">
+        <el-input v-model="dialogs.json.date" placeholder="左下角" />
+      </el-form-item>
+      <el-form-item label="副次标题">
+        <el-input v-model="dialogs.json.itude" placeholder="右下角" />
+      </el-form-item>
+      <el-form-item>
         <el-button type="primary" @click="dialogs.check">确定</el-button>
-      </span>
-    </template> -->
+      </el-form-item>
+    </el-form>
+  </el-dialog>
+
+  <el-dialog v-model="preView" @close="PreViewClose" width="85%" title="保存">
+    <swiper>
+      <swiper-slide v-for="(create,index) of creates" :key="index">
+        <el-image :src="create"></el-image>
+      </swiper-slide>
+    </swiper>
   </el-dialog>
   <!-- <p v-text="log"></p> -->
   <!-- <el-image :src="logSrc"></el-image> -->
@@ -289,26 +330,12 @@ const save = () =>{
 </template>
 
 <style scoped>
-@font-face {
-  font-family: 'medium';
-  src: url(//cdn.jsdelivr.net/gh/WarmEpoch/mark/MiSans-Mediu.ttf);
-}
-
-@font-face {
-  font-family: 'regular';
-  src: url(./assets/MiSans-Regular.ttf);
-}
-
-@font-face {
-  font-family: 'semibold';
-  src: url(./assets/MiSans-Semibold.ttf);
-}
-
-p {
+@import "https://font.sec.miui.com/font/css?family=MiSans:300,450,500,650:Chinese_Simplify,Latin&display=swap";
+p,.el-empty :deep(p){
   padding: 0;
   margin: 0;
-  outline: none;
   white-space: nowrap;
+  font-family: MiSans;
 }
 
 a{
@@ -316,8 +343,19 @@ a{
   color: unset;
 }
 
+
+.el-form :deep(.el-form-item__content){
+  flex-wrap: nowrap;
+  justify-content: flex-end;
+}
+
+.el-form .el-select--large{
+  flex: 1;
+}
+
 .el-image {
   display: block;
+  font-size: 100%;
 }
 
 .el-card__header .el-image {
@@ -334,7 +372,7 @@ a{
   background: #f5f5f5;
 }
 
-.avatar-uploader .el-button {
+.el-card :deep(.el-button) {
   margin-left: 1rem;
 }
 
@@ -342,5 +380,6 @@ a{
   display: flex;
   align-items: center;
 }
+
 
 </style>
